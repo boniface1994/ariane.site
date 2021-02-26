77bf6102046732aba363ac6a323054ecf8839121
Celaneo <admin@celaneo.com>
2021-02-26 12:40:51 +0300

    Refs #18249 - Sc Interface

--- routes/admin.php
+++ routes/admin.php
@@ -9,4 +9,9 @@ Route::group(['namespace' => 'Auth'], function(){
 
 Route::group(['middleware' => 'admin'], function(){
     Route::get('/', 'Dashboard\DashboardController@index')->name('admin.home');
+
+    Route::group(['prefix'=> 'configurator', 'namespace' => 'Configurator'], function() {
+    	Route::get('/scinterface', 'ScInterfaceController@index')->name('scinterface.home');
+    });
+    
 });
